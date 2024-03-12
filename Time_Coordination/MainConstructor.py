fe2d15e179616f3a48c595e29ae597b375e439a8
Eric <khellaeric@gmail.com>
2024-03-12 16:44:35 +0100

    Writing functions for each treatment (speed, time, km)

--- Time_Coordination/MainConstructor.py
+++ Time_Coordination/MainConstructor.py
@@ -15,6 +15,9 @@ class Video(object):
         """
         Returns frames, frame_number, fps, frame_size
         """
+        global frame_id
+        global total_frames
+        global frame
 
         Ti = t.time()
 
@@ -26,7 +29,7 @@ class Video(object):
 
         frames = []
         frame_id = 0
-        frame_decimation = 100 # Analyse every frame of this video
+        frame_decimation = 100 # Analyse every 100 frame of this video
         total_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
         if not capture.isOpened():
@@ -35,46 +38,32 @@ class Video(object):
         else:
             fps = capture.get(cv2.CAP_PROP_FPS)
             frame_dimensions=[int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))] # [WIDTH, HEIGH]
+
             while True:
                 success, frame = capture.read()
                 if success:
                     frames.append(Frame(id, np.array(frame)))
 
                     if frame_id%frame_decimation==0:
-                        speed_zone = frame[-h_speed:, :w_speed]
-                        speed_gray= cv2.cvtColor(speed_zone, cv2.COLOR_BGR2GRAY)
-                        speed_text= pytesseract.image_to_string(speed_gray, config=speed_config)
-                        speed = ''.join(filter(str.isdigit,speed_text))
-
-                        time_zone = frame[:h_time, w_time:]
-                        time_gray = cv2.cvtColor(time_zone, cv2.COLOR_BGR2GRAY)
-                        time_text = pytesseract.image_to_string(time_gray, config=time_config)
-                        time = ''.join(str(time_text)).strip()
-
-                        km_zone = frame[:h_km, w_km_s:w_km_e]
-                        km_gray = cv2.cvtColor(km_zone, cv2.COLOR_BGR2GRAY)
-                        km_text = pytesseract.image_to_string(km_gray, config=km_config)
-                        km = ''.join(str(km_text)).strip()
+                        speed_treatment()
+                        time_treatment()
+                        km_treatment()
 
                         writer.writerow([frame_id, speed, time, km])
 
                     frame_id += 1
-
-                    progress = frame_id/total_frames
-                    bar_length = 50
-                    block= int(round(bar_length*progress))
-                    progress_text= "\rProgress: [{0}] {1:.2f}% ({2}/{3} frames)".format("#" * block + "-" * (bar_length - block), progress * 100, frame_id, total_frames)
-                    sys.stdout.write(progress_text)
-                    sys.stdout.flush()
+                    progress_bar()
 
                 else:
                     print(mess.P_getvid, end='')
                     break
+
             capture.release()
             cv2.destroyAllWindows()
             file.close()
             Tf =t.time()
             print("\rThis code took {0} to excecute ".format(convert_ms_to_time_format((Tf-Ti)*1000)))
+            
             return frames, frame_id, fps, frame_dimensions
     
     
@@ -95,7 +84,43 @@ def convert_ms_to_time_format(ms):
 
 def get_frame_time(frame_index, fps):
         """
-        Returns time in ms"""
+        Returns time in ms
+        """
         initial_time = 0 # TBD
         time_in_ms = (frame_index / fps) * 1000
         return time_in_ms
+
+def progress_bar():
+    """
+    Prints the progress bar of the video treatment
+    """
+    progress = frame_id/total_frames
+    bar_length = 50
+    block= int(round(bar_length*progress))
+    progress_text= "\rProgress: [{0}] {1:.2f}% ({2}/{3} frames)".format("#" * block + "-" * (bar_length - block), progress * 100, frame_id, total_frames)
+    sys.stdout.write(progress_text)
+    sys.stdout.flush()
+
+def speed_treatment():
+    global speed
+
+    speed_zone = frame[-h_speed:, :w_speed]
+    speed_gray= cv2.cvtColor(speed_zone, cv2.COLOR_BGR2GRAY)
+    speed_text= pytesseract.image_to_string(speed_gray, config=speed_config)
+    speed = ''.join(filter(str.isdigit,speed_text))
+
+def time_treatment():
+    global time
+
+    time_zone = frame[:h_time, w_time:]
+    time_gray = cv2.cvtColor(time_zone, cv2.COLOR_BGR2GRAY)
+    time_text = pytesseract.image_to_string(time_gray, config=time_config)
+    time = ''.join(str(time_text)).strip()
+
+def km_treatment():
+    global km
+
+    km_zone = frame[:h_km, w_km_s:w_km_e]
+    km_gray = cv2.cvtColor(km_zone, cv2.COLOR_BGR2GRAY)
+    km_text = pytesseract.image_to_string(km_gray, config=km_config)
+    km = ''.join(str(km_text)).strip()
