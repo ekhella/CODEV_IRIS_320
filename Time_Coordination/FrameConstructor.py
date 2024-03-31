bc1a7a05a2413043c89ebad84ca22b8c4a99e160
Eric <khellaeric@gmail.com>
2024-03-31 11:41:07 +0200

    Creating the basement of the Time class

--- Time_Coordination/FrameConstructor.py
+++ Time_Coordination/FrameConstructor.py
@@ -1,4 +1,5 @@
+""" This code will be used to regroup Frames with their mesures and possibly applying image treatment on it"""
+
 class Frame(object):
-     def __init__(self, id, array):
-        self.id = id
-        self.array = array
+     def __init__(self):
+        self.id = None
--- Time_Coordination/TimeConstructor.py
+++ Time_Coordination/TimeConstructor.py
@@ -1,8 +1,13 @@
 """ This code will be a class where the input is a time and the output a Directory with frames corresponding to the input"""
-def get_frame_time(frame_index, fps): #UNUSED YET
-    """
-    Returns time in ms
-    """
-    initial_time = 0 # TBD
-    time_in_ms = (frame_index / fps) * 1000
-    return initial_time+time_in_ms
+class Time:
+    def __init__(self, time):
+        self.id = None
+        self.time= time
+
+    def get_frame_time(frame_index, fps): #UNUSED YET
+        """
+        Returns time in ms
+        """
+        initial_time = 0 # TBD
+        time_in_ms = (frame_index / fps) * 1000
+        return initial_time+time_in_ms
