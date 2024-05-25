06d37ebf163dc5bc08fd84aec37cb7e5863483d1
Eric <khellaeric@gmail.com>
2024-05-26 01:55:42 +0200

    Commenting all methods

--- Time_Coordination/VideoConstructor.py
+++ Time_Coordination/VideoConstructor.py
@@ -33,12 +33,22 @@ class VideoProcessor:
 
     @staticmethod
     def convert_ms_to_time_format(ms):
+        """
+        Converts milliseconds to time format
+        Input : Time in ms
+        Output : Time in HH:MM:SS:XXXX
+        """
         hours, ms = divmod(ms, 3600000)
         minutes, ms = divmod(ms, 60000)
         seconds, ms = divmod(ms, 1000)
         return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}:{int(ms):03}"
     
     def progress_bar(self, start_time):
+        """
+        Prints the progress of the analysis in % and the estimated time left
+        Input : start_time in ms
+        Output : Progres bar with % and estimated time left
+        """
         current_time = t.time()
         elapsed_time = current_time - start_time
         progress = self.frame_id / self.total_frames
@@ -50,6 +60,11 @@ class VideoProcessor:
         sys.stdout.flush()
 
     def measure_time(method):
+        """
+        Wrapped method to calculate the time of execution of the method in entry
+        Input : The method we want to time
+        Output : The output of the method; but it added to a self dict the running time of the entry
+        """
         def wrapper(self, *args, **kwargs):
             start_time = t.time()
             result = method(self, *args, **kwargs)
@@ -60,6 +75,11 @@ class VideoProcessor:
 
     @measure_time
     def open_video(self):
+        """
+        Opens a video
+        Input : None
+        Output : True if the Video is correctly opened; False otherwise
+        """
         self.capture = cv2.VideoCapture(self.video_path)
         if not self.capture.isOpened():
             print(mess.P_open, end='')
@@ -68,6 +88,11 @@ class VideoProcessor:
 
     @measure_time
     def get_video_info(self):
+        """
+        Gets the FPS, Dimensions and Total Frames of a Video
+        Input : None
+        Output : None
+        """
         self.fps = self.capture.get(cv2.CAP_PROP_FPS)
         self.frame_dimensions = [int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH)), 
                                  int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))]
@@ -75,10 +100,25 @@ class VideoProcessor:
         return None
 
     def read_frame(self):
+        """
+        Reads a single frame from the video capture object.
+        Input : None
+        Output:
+        - frame: The next frame in the video or None if no more frames are available.
+        """
         success, frame = self.capture.read()
         return frame if success else None
 
     def detect_change(self, current_zone, prev_zone, threshold=0):
+        """
+        Detects changes between two images (zones) using simple image processing techniques.
+        Inputs:
+        - current_zone: The current frame zone to analyze.
+        - prev_zone: The previous frame zone for comparison.
+        - threshold: The threshold for change detection sensitivity.
+        Output:
+        - Boolean indicating whether a change was detected based on the threshold.
+        """
         if prev_zone is None:
             return True
         current_gray = cv2.cvtColor(current_zone, cv2.COLOR_BGR2GRAY)
@@ -89,6 +129,23 @@ class VideoProcessor:
         return change_percent > threshold
 
     def extract_data_from_frame(self, frame):
+        """
+    Processes a single frame to extract relevant information based on predefined zones for speed, marker data, and time. 
+    Also logs changes detected between the current frame and previous frames for each zone.
+    
+    Inputs:
+    - frame: The current video frame to process.
+    
+    Outputs:
+    - data: Dictionary containing extracted information for the current frame including speed, markers, and time.
+    
+    Operations:
+    - For each zone defined (speed, km marker, meters, hours, minutes, seconds), the method:
+      * Detects changes from the previous frame using the detect_change method.
+      * Extracts text using OCR if a change is detected or reuses text from the previous frame if no change is detected.
+      * Logs change detections for each zone to analyze how often changes occur.
+    - Calculates and records the time taken to process each zone and the total extraction time for performance analysis.
+    """
         T_extraction_start = t.time()
         data = {}
         zones = {
@@ -111,11 +168,13 @@ class VideoProcessor:
                     self.prev_data[key] = {}
                 sub_data = {}
                 for subkey, (zone, config, label) in value.items():
-                    if label not in self.prev_data or self.detect_change(zone, self.prev_data[label][0]):
+                    change_detected = self.detect_change(zone, self.prev_data.get(label, (None, None))[0])
+                    self.change_log[label].append(change_detected)
+                    if label not in self.prev_data or change_detected:
                         text = self.get_text(zone, config)
-                        self.prev_data[label] = (zone, text)  # Store the zone for comparison and text for reuse
+                        self.prev_data[label] = (zone, text) 
                     else:
-                        text = self.prev_data[label][1]  # Reuse the text without recomputation
+                        text = self.prev_data[label][1]  
                     sub_data[subkey] = text
                 if key == 'time':
                     data[key] = "{}:{}:{}".format(sub_data['hour'], sub_data['minute'], sub_data['second'])
@@ -124,11 +183,14 @@ class VideoProcessor:
                 self.timings['extraction_' + key] += t.time() - start_extraction
             else:
                 zone, config, label = value
-                if label not in self.prev_data or self.detect_change(zone, self.prev_data[label][0]):
+                change_detected = self.detect_change(zone, self.prev_data.get(label, (None, None))[0])
+                self.change_log[label].append(change_detected)
+                if label not in self.prev_data or change_detected:
+                #if label not in self.prev_data or self.detect_change(zone, self.prev_data[label][0]):
                     text = self.get_text(zone, config)
-                    self.prev_data[label] = (zone, text)  # Store the zone for comparison and text for reuse
+                    self.prev_data[label] = (zone, text)  
                 else:
-                    text = self.prev_data[label][1]  # Reuse the text without recomputation
+                    text = self.prev_data[label][1] 
                 data[key] = text
                 self.timings['extraction_' + key] += t.time() - start_extraction
 
@@ -138,6 +200,14 @@ class VideoProcessor:
         return data
 
     def get_text(self, zone, config):
+        """
+        Extracts text from a given image zone using OCR configured by the specified configuration.
+        Inputs:
+        - zone: Image region from which to extract text.
+        - config: Configuration parameters for pytesseract.
+        Output:
+        - Extracted text as a string.
+        """
         gray = cv2.cvtColor(zone, cv2.COLOR_BGR2GRAY)
         _, bw = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
@@ -145,6 +215,12 @@ class VideoProcessor:
         return pytesseract.image_to_string(morphed, config=config).strip()
 
     def save_data(self, data, format_type):
+        """
+        Saves processed data into the specified format (CSV, dict, or list).
+        Inputs:
+        - data: The processed data to save.
+        - format_type: The format in which to save the data ('csv', 'dict', 'list').
+        """
         T_saving_start = t.time()
         if format_type == 'csv':
             if 'file' not in self.prev_data:  #
@@ -162,16 +238,26 @@ class VideoProcessor:
                 self.prev_data['file'].write(str([data['speed'], data['time'], data['marker']]) + '\n')
         self.timings['saving'] += t.time() - T_saving_start
 
-    def save_changes(self):
-        self.change_log['speed'].append(self.detect_change(self.speed_zone, self.prev_data.get('speed', (None, None))[0]))
-        self.change_log['km'].append(self.detect_change(self.km_zone, self.prev_data.get('km', (None, None))[0]))
-        self.change_log['m'].append(self.detect_change(self.m_zone, self.prev_data.get('m', (None, None))[0]))
-        self.change_log['hour'].append(self.detect_change(self.hour_zone, self.prev_data.get('hour', (None, None))[0]))
-        self.change_log['minute'].append(self.detect_change(self.minute_zone, self.prev_data.get('minute', (None, None))[0]))
-        self.change_log['second'].append(self.detect_change(self.second_zone, self.prev_data.get('second', (None, None))[0]))
-
-
     def process_video(self):
+        """
+    Processes the entire video, extracting data from each frame and saving it in the selected format.
+    It also manages the workflow for opening the video, reading frames, handling data extraction, 
+    saving data, and cleaning up resources after processing.
+    
+    Inputs:
+    - None directly, but relies on user input to select the output format (csv, dict, or list).
+    
+    Operations:
+    - Opens the video file and retrieves video info.
+    - Reads frames in a loop until the video ends.
+    - For each frame, it extracts data, saves it, logs change detections, and updates the progress bar.
+    - After all frames are processed, it cleans up resources and displays the results.
+    
+    Side effects:
+    - Depending on the selected format, data is written to a CSV file or a text file, or simply stored in memory.
+    - Generates a pie chart showing the proportion of time spent on various operations.
+    - Outputs a change detection plot to visualize changes across frames.
+    """
         format_type = input("Choose output format (csv, dict, list): ")
         start_time = t.time()
         if not self.open_video():
@@ -185,16 +271,18 @@ class VideoProcessor:
 
             data = self.extract_data_from_frame(frame)
             self.save_data(data, format_type)
-            self.save_changes()
             self.progress_bar(start_time)
             self.frame_id += 1
 
         self.cleanup()
-        self.plot_change_detection(self.video_processor.change_log)
+        self.display_changes(self.change_log)
         self.display_results(format_type)
 
     @measure_time
     def cleanup(self):
+        """
+        Closes any resources held by the VideoProcessor, specifically the video file and any open file handles.
+        """
         if 'file' in self.prev_data:
             self.prev_data['file'].close()
         self.capture.release()
@@ -225,6 +313,11 @@ class VideoProcessor:
         plt.show()
     
     def display_changes (self, results):
+        """
+        Displays a plot of detected changes over the course of the video processing.
+        Inputs:
+        - results: Dictionary containing lists of change detection results for each label.
+        """
         _ , ax = plt.subplots(figsize=(10, 6))
         for key, changes in results.items():
             ax.plot(changes, label=key)
