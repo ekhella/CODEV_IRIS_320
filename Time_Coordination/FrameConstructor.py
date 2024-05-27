c8edd2f2bb9c989207dd70ec5f726dcc4446d4e1
Eric <khellaeric@gmail.com>
2024-05-27 22:06:27 +0200

    Renaming and cleaning

--- Time_Coordination/FrameConstructor.py
+++
@@ -1,6 +0,0 @@
-""" This code will be used to regroup Frames with their mesures and possibly applying image treatment"""
-
-class Frame(object):
-     def __init__(self, id, array):
-        self.id = id
-        self.array = array
