2b2adc15398cd6c4dd6b3699e1451c56009c7b4e
Eric <khellaeric@gmail.com>
2024-05-27 23:37:18 +0200

    Few corrections

--- Time_Coordination/VideoConstructor.py
+++ Time_Coordination/VideoConstructor.py
@@ -296,7 +296,7 @@ class VideoProcessor:
         print("Timing Summary:")
         for key, value in self.timings.items():
             print(f"{key}: {self.convert_ms_to_time_format(value * 1000)}")
-        print(f"Total Duration:", self.total_duration)
+        print(f"Total Duration of the video:", self.convert_ms_to_time_format(self.total_duration*1000))
         print(f"The time ratio (duration/execution) is : ", self.timings['extraction_total']/self.total_duration)
         labels = []
         sizes = []
