d13c7c3e2ad7f083fc2d205f135b9cd54e19cb63
Eric <khellaeric@gmail.com>
2024-05-28 03:24:58 +0200

    Saving Data in Dir

--- Time_Coordination/VideoDirectoryTool.py
+++ Time_Coordination/VideoDirectoryTool.py
@@ -4,29 +4,27 @@ from Base import mess
 class PathManager:
     def __init__(self, user):
         self.user = user
-        self.setup_paths()
+        self.desktop=self.get_desktop_path()
 
-    def setup_paths(self):
+    def get_desktop_path(self):
         if os.name == 'nt':
-            self.desktop = f'C:/Users/{self.user}/Desktop'
+            return f'C:/Users/{self.user}/Desktop'
         elif os.name == 'posix':
-            self.desktop = f'/Users/{self.user}/Desktop'
+            return f'/Users/{self.user}/Desktop'
         else:
             raise Exception("Unsupported OS")
-        self.tempdir = f'{self.desktop}/Results'
-        self.videoStorage = f'{self.desktop}/.##Results##'
-        self.create_dir(self.tempdir)
-        self.create_dir(self.videoStorage)
-
-    def create_dir(self, dir_path):
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
 
     def delete_dir(self, dir_path):
         if os.path.exists(dir_path):
             sht.rmtree(dir_path)
 
-    def videoinput(self):
+    def get_or_create_dir(self, dir_name):
+        dir_path = f"{self.desktop}/{dir_name}"
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        return dir_path
+
+    #def videoinput(self):
         while True:
             if os.listdir(self.tempdir):
                 break
@@ -36,7 +34,7 @@ class PathManager:
         if len(files) != 1:
             print(mess.P_vi2, end='')
             self.delete_dir(self.tempdir)
-            self.create_dir(self.tempdir)
+            self.get_or_create_dir_dir(self.tempdir)
             return self.videoinput()
 
         filename = files[0]
@@ -57,7 +55,7 @@ class PathManager:
             return self.videoinput()
 
 #if __name__ == "__main__":
-user = gp.getuser()
-path_manager = PathManager(user)
+#user = gp.getuser()
+#path_manager = PathManager(user)
 #video_path = path_manager.videoinput()
 #print(f"Video is ready at {video_path}")
--- Time_Coordination/VideoFrameDisplay.py
+++ Time_Coordination/VideoFrameDisplay.py
@@ -1,4 +1,4 @@
-from Modules import np, cv2
+from Modules import np, cv2, os, gp, sys
 from VideoDataAnalyzer import VideoDataAnalyzer
 from VideoDirectoryTool import PathManager
 
@@ -7,23 +7,25 @@ class VideoFrameDisplay:
         self.video_path = video_path
         self.data_analyzer = VideoDataAnalyzer(data_path)
         self.video_cap = cv2.VideoCapture(video_path)
+        self.path_manager = PathManager(user=gp.getuser())
+        self.frame_dir = self.path_manager.get_or_create_dir('SavedFrames')
         if not self.video_cap.isOpened():
             raise ValueError("Error opening video file")
 
     def display_frame_for_time(self, time_input):
         frame_number = self.data_analyzer.get_frame_number(time_input)
-        print(frame_number)
         if frame_number is None:
             print("Could not calculate frame number from time input.")
-            return
-
-
+            return None
         self.video_cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
         ret, frame = self.video_cap.read()
         if ret:
+            frame_path = os.path.join(self.frame_dir, f"frame_at_{time_input}s.png")
+            cv2.imwrite(frame_path, frame)
             cv2.imshow(f'Frame for Time: {time_input}s', frame)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
+            print(f"Frame saved at {frame_path}")
         else:
             print("Failed to retrieve the frame.")
 
@@ -35,6 +37,9 @@ video_path = "Data_confidential/video_arriere.mp4"
 data_path = "videotreatment.txt"
 
 video_display = VideoFrameDisplay(video_path, data_path)
+
+sys.stderr = open(os.devnull, 'w') #Deletes the secure warning message
+sys.stderr = sys.__stderr__
 
 time_input = int(input("Time :"))
 video_display.display_frame_for_time(time_input)
